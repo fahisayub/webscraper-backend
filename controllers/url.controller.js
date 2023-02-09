afbe02e056ee19a00524e3ff59d5da9e0783a6b8
MUHAMMED FAHIZ P A <fahisayub@gmail.com>
2023-02-09 20:53:27 +0530

    fix:update controller

--- controllers/url.controller.js
+++ controllers/url.controller.js
@@ -11,38 +11,43 @@ const scrapUrl = async (req, res) => {
   try {
     const scrap = spawn('curl', ['-s', url]);
     let result = '';
-    scrap.stdout.on('data', ans => {
-      result += ans.toString();
+    scrap.stdout.on('data', data => {
+      result += data.toString();
+    });
+    scrap.stderr.on('data', (data) => {
+      console.error(`stderr: ${data}`);
+    });
+
+    scrap.on('close', (code) => {
       const data = extractor(result);
       const textdata = data.text;
       const media = data?.videos;
       data.image && media.push({ src: data.image });
       const links = data?.links;
-         fs.writeFile('scraptext.txt', textdata, () => {
-          
-          const wc =  spawn('wc', ['-w', 'scraptext.txt'])
-          let val = '';
-          wc.stdout.on('data', data => {
-            val = data.toString();
-          });
-          wc.stderr.on('data', (data) => {
-            console.error(`stderr: ${data}`);
-          });
-          wc.on('close',  (code) => {
+      fs.writeFile('scraptext.txt', textdata, () => {
+
+        const wc = spawn('wc', ['-w', 'scraptext.txt'])
+        let val = '';
+        wc.stdout.on('data', data => {
+          val = data.toString();
+        });
+        wc.stderr.on('data', (data) => {
+          console.error(`stderr: ${data}`);
+        });
+        wc.on('close', async (code) => {
+          try{
+
             payload.media = media;
             payload.links = links;
             payload.count = parseInt(val.split(' ')[0], 10);
             console.log(`child process exited with code ${code}`);
-          });
+            await UrlModel.insertMany([{ ...payload }]);
+            res.send(payload);
+          }catch(err){
+            console.log(err);
+          }
         });
       });
-      scrap.stderr.on('data', (data) => {
-        console.error(`stderr: ${data}`);
-      });
-      
-      scrap.on('close', async(code) => {
-        await UrlModel.insertMany([{ ...payload }]);
-      res.send(payload);
 
       console.log(`child process exited with code ${code}`);
     });
@@ -70,24 +75,24 @@ const updateUrl = async (req, res) => {
 const deleteUrl = async (req, res) => {
   let { id } = req.params;
   try {
-     await UrlModel.deleteOne({ _id: id });
+    await UrlModel.deleteOne({ _id: id });
     res.send({ msg: "Url deleted successfully" });
   } catch (err) {
     res.send({ msg: "Something went wrong", error: err });
   }
 };
-const getHistory=async(req,res)=>{
-  try{
-    let history=await UrlModel.find({});
-    res.send({ msg: "History data",data:history})
-  }catch(e){
-    res.send({ msg: "Something went wrong", error:e.message });
+const getHistory = async (req, res) => {
+  try {
+    let history = await UrlModel.find({});
+    res.send({ msg: "History data", data: history })
+  } catch (e) {
+    res.send({ msg: "Something went wrong", error: e.message });
   }
-  
+
 }
 
 const urlController = {
-  scrapUrl,updateUrl,deleteUrl,getHistory
+  scrapUrl, updateUrl, deleteUrl, getHistory
 
 };
 module.exports = {
